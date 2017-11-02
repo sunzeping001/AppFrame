f02b85a9a60250e36148d4444c6873b0a632a056
sunzeping <233558620@qq.com>
2017-11-02 23:58:02 +0800

    增加新闻的mock数据，并且修复SliderAdapter加载更多错误的问题

--- app/src/main/java/com/szp/app/frame/AppAplication.java
+++ app/src/main/java/com/szp/app/frame/AppAplication.java
@@ -2,17 +2,25 @@ package com.szp.app.frame;
 
 import android.app.Application;
 
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactNativeHost;
+
+
 /**
  * Created by sunzeping on 17/9/29.
- * Function :
+ * Function : 实现了ReactApplication类的
  * Desc :
  */
 
-public class AppAplication extends Application {
+public class AppAplication extends Application implements ReactApplication {
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
 	}
 
+	@Override
+	public ReactNativeHost getReactNativeHost() {
+		return null;
+	}
 }
